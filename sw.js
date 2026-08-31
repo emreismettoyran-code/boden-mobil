b6da9549adb4ef71e01722d85ece8fab26e19e8f
emreismettoyran-code <emreismettoyran@gmail.com>
2026-09-01 00:30:12 +0300

    Eski uygulama önbelleğini temizle ve güncellemeleri zorunlu yükle

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-const CACHE='boden-v1';const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{let c=x.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return x}).catch(()=>caches.match('./index.html'))))});
+// Güncelleme sonrası eski sayfaların önbellekten gelmesini engelle
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',event=>event.waitUntil((async()=>{await self.clients.claim();const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)));})()));
+self.addEventListener('fetch',event=>{if(event.request.method==='GET')event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));});
